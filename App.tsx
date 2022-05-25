12d288d0c2f5ffa170a58336e16c0511752d4941
ana monteiro <anamonteiro430@gmail.com>
2022-05-25 23:56:03 +0100

    react native web setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PersonalInfo />
     </View>
   );
 }
@@ -13,8 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
